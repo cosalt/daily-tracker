c4924349df92a51545b0bb0151e26782bba92bf7
Ray Martin <cosalt@users.noreply.github.com>
2025-12-08 14:48:09 +0000

    Add Ray's solutions

--- Ray/Project Euler/id1.py
+++ Ray/Project Euler/id1.py
@@ -3,17 +3,9 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
-def fib(n):
+sum = 0
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        sum += i
 
-    if n in [1,2]:
-        return 1
-
-    return (fib(n-1) + fib(n-2))
-sum = 0    
-for i in range(2,1000):
-    if fib(i) < 4000000:
-        if (fib(i)%2) == 0:
-            sum += fib(i)
-    else:
-        break
 print(sum)
