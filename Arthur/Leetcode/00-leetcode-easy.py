17f3c7d7b08fbfa239558f3071ce859034aa41a9
Arthur Srodzinski-Stevens <126922564+arthurstevens@users.noreply.github.com>
2025-12-08 11:28:47 +0000

    docs(day 1): ammend name for day skip
    Added a comment about Day 00 in the leetcode problem.

--- Arthur/Leetcode/00-leetcode-easy.py
+++ Arthur/Leetcode/00-leetcode-easy.py
@@ -1,6 +1,7 @@
 # Problem: Count Odd Numbers in an Interval Range
 # Link: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
 # Notes: Both solsrun at effectively same speed (got better results on mine? but shouldn't)
+#        Day 00 because Ray fucked it up
 
 class Solution:
     # My sol
